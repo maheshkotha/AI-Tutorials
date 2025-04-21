0dfcd75f4cfdec75205978401d95b2920d8a8d5e
Kotha Mahesh Babu <kotha_maheshbabu@epam.com>
2025-04-21 11:57:41 +0530

    changes

--- Webpack-AI/src/index.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello Webpack");
-
-function addTwoNumbers(a, b) {
-  return a + b
-}
-
-console.log("Hellow Webpack Dev server");
-
-document.body.innerHTML = `<h1> Hot Module Replacement is Live</h1>`
